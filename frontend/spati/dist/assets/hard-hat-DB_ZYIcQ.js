6189c85456751dc681f631c547189e33bd1260b4
miguellucero123 <miguellucerogatica@gmail.com>
2026-07-29 14:36:05 -0400

    chore(spati): dist panel aviso NWP

--- frontend/spati/dist/assets/hard-hat-DB_ZYIcQ.js
+++ frontend/spati/dist/assets/hard-hat-DB_ZYIcQ.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-dEZIZLwn.js";/**
+import{c as a}from"./index-BfqlDiMu.js";/**
  * @license lucide-vue-next v0.574.0 - ISC
  *
  * This source code is licensed under the ISC license.
