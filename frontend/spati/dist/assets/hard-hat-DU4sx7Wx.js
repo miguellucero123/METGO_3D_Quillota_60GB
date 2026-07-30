90bd99a37b8dbd2e9f580e62817df9d6e9e4c742
miguellucero123 <miguellucerogatica@gmail.com>
2026-07-29 23:35:19 -0400

    chore(spati): dist umbrales alertas M9

--- frontend/spati/dist/assets/hard-hat-DU4sx7Wx.js
+++ frontend/spati/dist/assets/hard-hat-DU4sx7Wx.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-BCbnHXDY.js";/**
+import{c as a}from"./index-B7x2Fo8K.js";/**
  * @license lucide-vue-next v0.574.0 - ISC
  *
  * This source code is licensed under the ISC license.
